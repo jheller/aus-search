80446cbe9187fa2734d7db74c14fc3e7af399ee3
John Heller <joheller@deloitte.com.au>
2019-06-07 16:35:48 +1000

    Update to latest database zip file download

--- download.sh
+++ download.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget -O ./data/gnaf-latest.zip https://data.gov.au/dataset/19432f89-dc3a-4ef3-b943-5326ef1dbecc/resource/4b084096-65e4-4c8e-abbe-5e54ff85f42f/download/may18gnafpipeseparatedvalue20180521161504.zip
+wget -O ./data/gnaf-latest.zip https://data.gov.au/data/dataset/19432f89-dc3a-4ef3-b943-5326ef1dbecc/resource/4b084096-65e4-4c8e-abbe-5e54ff85f42f/download/may19_gnaf_pipeseparatedvalue_20190521155815.zip
 mkdir ./data/tmp
 unzip -o ./data/gnaf-latest.zip -d ./data/tmp
 rm -f ./data/*.psv
